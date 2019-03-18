809c1fda0d8e14bbf74c8d692a4f2b25161c5ac3
Royal-Code <tomburridge2002@gmail.com>
2019-03-18 20:02:24 +0000

    Add More

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const exphbs  = require('express-handlebars');
+
+const passport      = require('passport');
+
+const app = express();
+const port = 8080;
+
+
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public/'));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', (req, res) => {
+    res.render("index");
+});
+
+app.post('/', (req, res) => {
+
+});
+
+app.listen(port, () => {
+    console.log("Server Started on Port: " + port);
+});
+
+
+
+
